6e80dcacfe6285eea90dbfeb955d4f873f6d41e3
Osvaldo Mesa <d3v.ozzy@gmail.com>
2024-10-20 20:47:10 -0700

    add boundary hit logic and stop snake

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
 import { useEffect, useState } from 'react'
 import './App.css'
 
+const boundaryRight = new Map()
+boundaryRight.set(10, true)
+
 function App() {
   const [snake, setSnake] = useState([0])
 
@@ -8,18 +11,20 @@ function App() {
     const timeoutId = setTimeout(() => {
       const tail = snake.length - 1
       const newValue = ++snake[tail]
+      // console.log("snake: ", snake)
       snake.splice(0, 1)
-      setSnake([...snake, newValue])
+      if (!boundaryRight.has(newValue)) {
+        setSnake([...snake, newValue])
+      }
     }, 1000)
-
     return () => clearTimeout(timeoutId)
   }, [snake])
 
   const content = () => {
     const list = new Array(10)
     for (let i = 0; i < list.length; i++) {
-      const isSnake = snake.includes(i) ? 'blankSpace snake' : 'blankSpace'
-      list[i] = (<div key={i} className={isSnake}></div>)
+      const divStyle = snake.includes(i) ? 'blankSpace snake' : 'blankSpace'
+      list[i] = (<div key={i} className={divStyle}></div>)
     }
     return list
   }
