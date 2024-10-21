328a813274a25f8f371b9bd68399e208cb3d1c86
Osvaldo Mesa <d3v.ozzy@gmail.com>
2024-10-20 20:15:58 -0700

    create snake and move it across row
    Next I will address the snake hitting the wall and ending the
    game.

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [snake, setSnake] = useState([0])
+
+  useEffect(() => {
+    const timeoutId = setTimeout(() => {
+      const tail = snake.length - 1
+      const newValue = ++snake[tail]
+      snake.splice(0, 1)
+      setSnake([...snake, newValue])
+    }, 1000)
+
+    return () => clearTimeout(timeoutId)
+  }, [snake])
+
+  const content = () => {
+    const list = new Array(10)
+    for (let i = 0; i < list.length; i++) {
+      const isSnake = snake.includes(i) ? 'blankSpace snake' : 'blankSpace'
+      list[i] = (<div key={i} className={isSnake}></div>)
+    }
+    return list
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className='topLvl'>
+        {content()}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
