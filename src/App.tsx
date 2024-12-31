0e89987e597e627dba501450f2a1ebdaa47026a1
Osvaldo Mesa <d3v.ozzy@gmail.com>
2024-12-30 21:51:20 -0800

    add food style and state
    Additionally replaced single quotes to double quotes - java
    style

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,27 @@
-import { useState, } from 'react'
-import './App.css'
-import useUserInput from './hooks/useUserInput'
-import useAutoMove from './hooks/useAutoMove'
+import { useState, } from "react"
+import "./App.css"
+import useUserInput from "./hooks/useUserInput"
+import useAutoMove from "./hooks/useAutoMove"
 
 function App() {
   const [snake, setSnake] = useState([0])
+  const [food, setFood] = useState(190)
   const [direction, setDirection] = useState("right")
   useUserInput(setDirection)
   useAutoMove(snake, setSnake, direction)
 
-
+  //  TODO: how to randomly add food to the content, while ensuring it wont be on the snake
   const content = () => {
     const list = new Array(400)
     for (let i = 0; i < list.length; i++) {
-      const divStyle = snake.includes(i) ? 'blankSpace snake' : 'blankSpace'
+      let divStyle = null
+
+      if (i === food) {
+        divStyle = "space food"
+      } else if (snake.includes(i)) {
+        divStyle = "space snake"
+      } else divStyle = "space"
+
       list[i] = (<div key={i} className={divStyle}></div>)
     }
     return list
@@ -21,7 +29,7 @@ function App() {
 
   return (
     <>
-      <div className='topLvl'>
+      <div className="topLvl">
         {content()}
       </div>
     </>
